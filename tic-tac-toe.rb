70dc8373b76d3b2040cea45aa694951452af99ca
Rene <cheesus928@gmail.com>
2016-06-23 10:22:47 -0500

    made a few changes

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,50 +1,61 @@
 class Board
     def initialize (perfsqr)
-        @GRID = perfsqr
-        @SQRGRID = (@GRID**0.5).to_i
-        @board = Array.new(@GRID,nil)
+        @SQRGRID = (perfsqr**0.5).to_i
+        @board = Array.new(@SQRGRID) {|i| Array.new(@SQRGRID,nil)}
         self.display
     end
-    def place (m,element)
+    def place (m,index,element)
         puts
-        @board[element] = m
+        @board[index][element] = m
         self.display
     end
     def display
         i = 0
         @SQRGRID.times do
             puts
-            print @board[i,@SQRGRID]
-            i += @SQRGRID
+            print @board[i]
+            i += 1
             puts
         end
         self.check
     end
     def check
         i = 0
-        colnum = 1
-        check_columns = @board.select {|elem| @board.index(elem)%@SQRGRID == 0}
-        print check_columns
+        x = 0
+        column_index = 0
         @SQRGRID.times do
-            check_rows = @board[i,@SQRGRID]
+            check_rows = @board[i]
+            check_columns = []
+            right_diagonal = (0...@SQRGRID).collect {|i| @board[i][i]}
+            @SQRGRID.times do
+                check_columns.unshift(@board[x][column_index])
+                x += 1
+            end
             if check_rows.all? {|item| item == check_rows[0] && item != nil}
                 puts
                 puts "END OF GAME"
                 puts "#{check_rows[0]} WINS"
                 break
-            elsif check_columns.all? {|item| item == check_columns[0] && item != nil && check_columns.count == @SQRGRID}
+            elsif check_columns.all? {|item| item == check_columns[0] && item != nil}
                 puts
                 puts "END OF GAME"
                 puts "#{check_columns[0]} WINS"
                 break
+            elsif right_diagonal.all? {|item| item == right_diagonal[0] && item != nil}
+                puts
+                puts "END OF GAME"
+                puts "#{right_diagonal[0]} WINS"
+                break
             else
-                i += @SQRGRID
+                i += 1
+                x = 0
+                column_index += 1
             end
         end
     end
 end
 
 a = Board.new(9)
-a.place(1,0)
-a.place(1,3)
-a.place(1,6)
+a.place(1,0,0)
+a.place(1,1,1)
+a.place(1,2,2)
