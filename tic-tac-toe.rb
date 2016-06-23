1d6e6e95c2ce2c7c4517a7b820fcaf788c8ddb1d
Rene <cheesus928@gmail.com>
2016-06-23 15:01:47 -0500

    Simplified the board, improved code

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,61 +1,105 @@
-class Board
-    def initialize (perfsqr)
-        @SQRGRID = (perfsqr**0.5).to_i
-        @board = Array.new(@SQRGRID) {|i| Array.new(@SQRGRID,nil)}
-        self.display
+class Game
+    attr_accessor :player1, :player2
+    def initialize
+        @grid = Board.new
+        @turn = 1
     end
-    def place (m,index,element)
+    def play
+        get_names
+        turn
+        has_won
+    end
+    def get_names
+        print  "Enter player1(X)'s name: "
+        self.player1 = gets.chomp
         puts
-        @board[index][element] = m
-        self.display
+        print "Enter player2(O)'s name: "
+        self.player2 = gets.chomp
     end
-    def display
-        i = 0
-        @SQRGRID.times do
-            puts
-            print @board[i]
-            i += 1
-            puts
+    def turn
+        if @turn == 1
+            print "#{self.player1}'s turn, type in a position (0-8): "
+            pos = gets.to_i
+            place(1,pos)
+            @turn += 1
+        else
+            print "#{self.player2}'s turn, type in a position (0-8): "
+            pos = gets.to_i
+            place(2,pos)
+            @turn -= 1
         end
-        self.check
     end
-    def check
-        i = 0
-        x = 0
-        column_index = 0
-        @SQRGRID.times do
-            check_rows = @board[i]
-            check_columns = []
-            right_diagonal = (0...@SQRGRID).collect {|i| @board[i][i]}
-            @SQRGRID.times do
-                check_columns.unshift(@board[x][column_index])
-                x += 1
-            end
-            if check_rows.all? {|item| item == check_rows[0] && item != nil}
-                puts
-                puts "END OF GAME"
-                puts "#{check_rows[0]} WINS"
-                break
-            elsif check_columns.all? {|item| item == check_columns[0] && item != nil}
-                puts
-                puts "END OF GAME"
-                puts "#{check_columns[0]} WINS"
-                break
-            elsif right_diagonal.all? {|item| item == right_diagonal[0] && item != nil}
-                puts
-                puts "END OF GAME"
-                puts "#{right_diagonal[0]} WINS"
-                break
+    
+    def place (player,pos)
+        @grid.place(player,pos)
+    end
+    def has_won
+        while @grid.won == false
+            turn
+        end
+        puts
+        puts "END OF GAME"
+    end
+end
+
+
+class Board
+    attr_accessor :won
+    def initialize
+        @board = Array.new(9,nil)
+        self.won = false
+    end
+    def place (player,pos)
+        @board[pos] = "X" if player == 1
+        @board[pos] = "O" if player == 2
+        show_board
+        check_wins
+    end
+    def show_board
+        print @board[0,3]
+        puts
+        print @board[3,3]
+        puts
+        print @board[6,3]
+        puts
+    end
+    def check_wins
+        check_rows
+        check_columns
+        check_diagonals
+    end
+    def check_rows
+        row_num = 0
+        3.times do
+            row = @board[row_num,3]
+            if row.all? {|i| i == row[0] && i != nil}
+                self.won = true
             else
-                i += 1
-                x = 0
-                column_index += 1
+                row_num += 3
             end
+        end
+    end
+    def check_columns
+        col_num = 0
+        3.times do
+            col = [@board[0+col_num],@board[3+col_num],@board[6+col_num]]
+            if col.all? {|i| i == col[0] && i != nil}
+                self.won = true
+            else
+                col_num += 1
+            end
+        end
+    end
+    def check_diagonals
+        right_diag = [@board[0],@board[4],@board[8]]
+        left_diag = [@board[2],@board[4],@board[6]]
+        if left_diag.all? {|i| i == left_diag[0] && i != nil}
+            self.won = true
+        elsif right_diag.all? {|i| i == right_diag[0] && i != nil}
+            self.won = true
         end
     end
 end
 
-a = Board.new(9)
-a.place(1,0,0)
-a.place(1,1,1)
-a.place(1,2,2)
+a = Game.new
+a.play
